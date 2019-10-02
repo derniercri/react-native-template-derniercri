f22dbeeebe9b3a0c538679cdebab0c08dace2206
Donovan BENFOUZARI <donovan@derniercri.com>
2019-10-02 16:54:52 +0200

    chore: add hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  plugins: ['react-native'],
+  extends: [
+    '@react-native-community',
+    'plugin:react-native/all',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  rules: {
+    'react-native/no-raw-text': 'off',
+    'import/namespace': 'off',
+    'import/named': 'off', // Typescript already checks for unresolved imports
+    'import/no-unresolved': 'off', // Typescript already checks for unresolved imports
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+  },
 };
